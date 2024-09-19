33e64435fff1129e8ea24ce42e927cce401b592e
Eddiel321 <EDdiel32@nycstudents.net>
2024-09-19 10:12:18 -0400

    updatebill.py

---
+++ bill.py
@@ -0,0 +1,4 @@
+#Let's create a function to accept a "bill" value and offer a tip of 0%, 15%, 20% or 25% depending on if the service was "bad, okay, good , or great ". 
+x=7
+x=input("how much was the bill?")
+input("bad,okay,good,great")
