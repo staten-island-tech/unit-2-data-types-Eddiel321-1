a36842ef22aa5059514850adbae255ead34231f9
Eddiel321 <EDdiel32@nycstudents.net>
2024-09-26 10:04:25 -0400

    999

---
+++ GCF.py
@@ -0,0 +1,7 @@
+a=72
+b= 86
+for i in range(2,a+1):
+    if a%i == "0": 
+    and if b%i=="0":
+a * b/4
+print(result) # output 2
--- bill.py
+++ bill.py
@@ -1,4 +1,3 @@
-x=15
 bad=0
 okay=15
 good=20
@@ -7,22 +6,23 @@ bill=input("how much is the bill?")
 print(bill)
 input("how was the service")
 values=(0,15,20,25)
+x=input("stick in number:)")
 print(values)
-if x ==0:
+if 14>x> 0-1:
     print("bad")
-if x==15:
+if 19>x>14:
     print("okay")
-if x==20:
+if 24>x>19:
     print("good")
-if x==25:
+if x>24:
     print("great")
 input("how much would you like tip?")
 print(values)
 if x == bad:
     print ("0% tip")
-elif x==okay:
-    input("15% tip")
-else :x== good
-input("20% tip")
-if x == great:                                              
-    input("25% tip")
+if x == okay:
+    print("15% tip")
+if x == good:
+    print("20% tip")
+if x == great:
+    print("25% tip")
