a6a369ecde0da1def9b397070f606705ac579735
Eddiel321 <EDdiel32@nycstudents.net>
2024-10-02 09:45:31 -0400

    Create MADLIBS.py

---
+++ MADLIBS.py
@@ -0,0 +1,7 @@
+noun1 = input("Enter a noun: ") 
+noun2 = input("Enter another noun: ") 
+food1 = input("Enter a food: ") 
+food2 = input("Enter another food: ") 
+
+story = "On " + noun1 + " day, " + noun2 + "'s lunch was stolen by a " + food1 + ". She chased it but tripped. Luckily, her friends shared their " + food2 + "."
+print(story)
